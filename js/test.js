62907d4388b479dc656676cb18b546810ba3a715
김경희 <boston_ivy@yonsei.ac.kr>
2022-10-25 17:47:58 +0900

    prototype

---
+++ js/test.js
@@ -0,0 +1,30 @@
+class classP {
+  constructor(a,b){
+      this.aa = a
+      this.bb = b
+  }
+}
+
+function funcP (a,b) {
+  this.aa = a
+  this.bb = b
+}
+
+classP.hi = function () {
+  console.log(`hi class ${this.aa}`)
+}
+
+funcP.hi = function () {
+  console.log(`hi func ${this.aa}`)
+}
+
+const constP = {
+  aa:'',
+  bb:'',
+  get sett() {
+    return this.aa, this.bb
+  },
+  set sett(ab) {
+    [this.aa, this.bb] = ab.split(" ")
+  }
+}
